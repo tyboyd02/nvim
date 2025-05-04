c558035ba7b7b7d35daf9348fc17a9c07f047140
Tyler Boyd <tyboyd02@gmail.com>
2025-05-04 17:32:54 -0400

    remove white space

--- lua/tyler/remap.lua
+++ lua/tyler/remap.lua
@@ -9,5 +9,3 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help ta
 -- Visual mode 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
-
-
