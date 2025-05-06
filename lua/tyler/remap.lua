55341a8a7908ea3d755282e57abb343689075bb5
tyler boyd <tyboyd02@gmail.com>
2025-05-06 12:12:46 -0400

    buffer list

--- lua/tyler/remap.lua
+++ lua/tyler/remap.lua
@@ -48,3 +48,4 @@ vim.keymap.set("n", "<leader>ga", function() vim.cmd("Git add %") end, { desc = 
 vim.keymap.set("n", "<leader>bn", ":bn<CR>", { desc = "Next buffer" })
 vim.keymap.set("n", "<leader>bp", ":bp<CR>", { desc = "Previous buffer" })
 vim.keymap.set("n", "<leader>bd", ":bd<CR>", { desc = "Delete buffer" })
+vim.keymap.set("n", "<leader>bl", ":buffers<CR>", { desc = "List buffers" })
